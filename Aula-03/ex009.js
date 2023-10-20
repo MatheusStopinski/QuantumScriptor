a526f65e84e4790a11226f0f2ece2495649e5e02
Matheus Stopinski <matheus.stopinski@hotmail.com>
2023-10-20 19:58:48 -0300

    File 'JS' 1º commit add

---
+++ Aula-03/ex009.js
@@ -0,0 +1,8 @@
+var velocidade = 90.6;
+console.log(`A velocidade de seu veiculo está em: ${velocidade}km/h`);
+
+if (velocidade > 85) {
+  console.log(`'if' fosse pelo ar seria mais rápido! Ass: Piloto de caça.`);
+}
+
+console.log(`Sempre deixe seu combutível cheio!`);
