764c73f456ed93cbf9415724558a2609d8a1ec64
Matheus Stopinski <matheus.stopinski@hotmail.com>
2023-10-30 02:06:29 -0300

    function contar( )

--- Aula-04/ex013.js
+++ Aula-04/ex013.js
@@ -1,6 +1,21 @@
-var c = 1   
-while (c <= 7) {
-    console.log(`Passo ${c}`)
-    c++
+function contar() {
     
+let inicio = window.document.querySelector('input#_nbsp')
+let fim = window.document.querySelector('input#nbsp')
+let passo = window.document.querySelector('input#passo')
+var contagem = window.document.querySelector('p.contagem')
+
+if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) { 
+    alert('[ERRO] Faltam informações!')
+} else {
+    contagem.innerHTML = 'Contando...'
+    let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    for (let c = i; c <= f; c += p ) {
+        contagem.innerHTML += ` ${c} \u{1F449}`  
+    }
+    contagem.innerHTML += `\u{1F3C1}`
+    
+}
 }
