8b701d8815eb2619955a4565ff5dd87f84d2752d
Matheus Stopinski <matheus.stopinski@hotmail.com>
2023-11-03 17:00:26 -0300

    Basic Fatorial Recursiva

---
+++ Aula-05/ex021.js
@@ -0,0 +1,11 @@
+// Fatorial Recursiva ( RECURSIVIDADE! Quando uma função chama ela mesma.)
+// n! = n * (n-1)   
+
+function fatorial(n) {
+    if (n == 1) {
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+console.log(fatorial(7));
