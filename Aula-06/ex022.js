b368994a0b720bfa777f38d91b929b527253cc14
Matheus Stopinski <matheus.stopinski@hotmail.com>
2023-11-06 00:09:33 -0300

    Analyzer and Validator

--- Aula-06/ex022.js
+++ Aula-06/ex022.js
@@ -1,4 +1,32 @@
 const btn = document.querySelector('.btn');
+let num = document.querySelector('input#between')
+let list = document.querySelector('select#seld')
+let resul = document.querySelector('div.resul')
+let values = []
+
+function add() {
+  if (isNumber(num.value) && !inList(num.value, values)) {
+    
+  } else {
+    alert('Valor inválido ou já encontrado.')
+  }
+}
+
+function isNumber(n) {
+  if (Number(n) >= 1 && Number(n) <= 100) {
+    return true
+  } else {
+    return false
+  }
+}
+function inList(n) {
+  if (l.indexOf(Number(n)) !== -1) {
+    return true
+  } else {
+    return false
+  }
+}
+
 
 btn.addEventListener('click', () => {
   btn.classList.add('active');
@@ -8,5 +36,7 @@ btn.addEventListener('click', () => {
     btn.classList.remove('active');
   }, 230);
 });
+
+
 
 
