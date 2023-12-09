b75ac39fa2016b99832a1560b765e701048b8489
Matheus Stopinski <matheus.stopinski@hotmail.com>
2023-12-08 23:57:02 -0300

    Aula_3_DIO

---
+++ DIO/aula_03.js
@@ -0,0 +1,11 @@
+const media = (5 + 3 + 7) / 3
+console.log(media);
+
+if (media < 5) {  
+  console.log('Reprovado');
+} else if (media >= 5 && media <= 7) {
+  console.log('Recuperação');
+} else {
+  console.log('Aprovado');
+}
+
