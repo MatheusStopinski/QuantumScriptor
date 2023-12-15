8494beb68b6d1fd90077d33fd78f9e1614577746
Matheus Stopinski <matheus.stopinski@hotmail.com>
2023-12-15 16:25:55 -0300

    AULA_8_DIO_PAY_FUNCTION

--- DIO/aula_08.js
+++ DIO/aula_08.js
@@ -1,12 +1,12 @@
 const price = 100
-const pay = '5'
+const pay = '4'
 
 function aplicarDesconto(valor, desconto) {
     return (valor - (valor * (desconto/100)));
 }
 
-function aplicarJuros(valor, desconto) {
-    return (valor + (valor * (desconto/100)));
+function aplicarJuros(valor, juros) {
+    return (valor + (valor * (juros/100)));
 }
 
 if (pay === '1') {
