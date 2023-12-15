d901575c0a8ebeff65005479b52da229bebe4d17
Matheus Stopinski <matheus.stopinski@hotmail.com>
2023-12-15 15:56:51 -0300

    Aula_7_DIO_UPDATED_IDADE+NOME

--- DIO/aula_07.js
+++ DIO/aula_07.js
@@ -1,16 +1,28 @@
-function escrevaNome(nome){
+// function escrevaNome(nome){
     
-    console.log('Meu nome é ' + nome);
-}
+//     console.log('Meu nome é ' + nome);
+// }
 
-escrevaNome('Matheus');
+// escrevaNome('Matheus');
+
+// function vereficarIdade(idade) {
+//     if (idade < 18) {
+//         console.log('Menor de idade');
+//     } else {
+//         console.log('Maior de idade');
+//     }
+// }
+//  vereficarIdade(18)
+
+function escrevaNome(nome){
+     return 'Seu nome é ' + nome;
+}
 
 function vereficarIdade(idade) {
     if (idade < 18) {
-        console.log('Menor de idade');
+        console.log(escrevaNome('Matheus ') + 'logo menor de idade');
     } else {
-        console.log('Maior de idade');
+        console.log(escrevaNome('Matheus ') + 'logo maior de idade');
     }
 }
-
  vereficarIdade(18)
