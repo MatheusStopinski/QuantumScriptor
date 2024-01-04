abc136aef4aeeeb93003defd2fbaf0edb81c5ce8
Matheus Stopinski <matheus.stopinski@hotmail.com>
2024-01-04 17:34:19 -0300

    AULA_17_DIO_EXERCICIO_NºPAR

---
+++ DIO/aula_17.js
@@ -0,0 +1,11 @@
+
+const lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i <= 10; i++) {
+    const numeros = lista[i];
+    
+    if (numeros % 2 === 0 ) {
+        console.log(numeros);
+    }
+    
+}
