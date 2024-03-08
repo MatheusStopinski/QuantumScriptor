f07125cb12ea55e026b5f10a60e845dda750192f
Matheus Stopinski <matheus.stopinski@hotmail.com>
2024-03-08 17:33:40 -0300

    add +1'then para pegar a lista dos pokemons

--- DIO/Criando um Site/assets/js/main.js
+++ DIO/Criando um Site/assets/js/main.js
@@ -4,13 +4,14 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 fetch(url) // get imbutido, fetch() tem um response. POR FIM É ASSINCRONO, DEMORA UM POUCO PRA VIR A RESPOSTA!
 .then((response) => response.json()) // AQUI É PARA O SUCESSO. Forma reduzida. 
-.then(function (jsonBody) { //AQUI VEM O 'RETORNO DO PRIMEIRO THEN'. Forma normal.
-    console.log(jsonBody);
+.then((jsonBody) => jsonBody.results) 
+.then((pokemonLista) => { //AQUI VEM O 'RETORNO DO PRIMEIRO THEN'. Forma normal.
+    console.log(pokemonLista);
 })
-
 .catch(function (error) { // FUNCÇÃO PARA O N SUCESSO
     console.log(error);
 })
 .finally(function () { //independente dos dois acima    
     console.log('Requisição concluída!');
 })
+
