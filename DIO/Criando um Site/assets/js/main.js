827d2e89190fa68dc18b6fbba58ef1bcbb3ccd5d
Matheus Stopinski <matheus.stopinski@hotmail.com>
2024-03-08 17:16:30 -0300

    exemplo de sintaxe reduzida

--- DIO/Criando um Site/assets/js/main.js
+++ DIO/Criando um Site/assets/js/main.js
@@ -1,1 +1,16 @@
-console.log('Sucesso...!');
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url) // get imbutido, fetch() tem um response. POR FIM É ASSINCRONO, DEMORA UM POUCO PRA VIR A RESPOSTA!
+.then((response) => response.json()) // AQUI É PARA O SUCESSO. Forma reduzida. 
+.then(function (jsonBody) { //AQUI VEM O 'RETORNO DO PRIMEIRO THEN'. Forma normal.
+    console.log(jsonBody);
+})
+
+.catch(function (error) { // FUNCÇÃO PARA O N SUCESSO
+    console.log(error);
+})
+.finally(function () { //independente dos dois acima    
+    console.log('Requisição concluída!');
+})
